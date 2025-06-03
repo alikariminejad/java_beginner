fd0c2b3e8ae2f9c63640b2fa2a5b2c7bb1462a4a
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-03 23:53:13 +0330

    first program with java

---
+++ Java_beginner/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // this is a comment
+        System.out.println("Hello World"); // you can use sout + Tab for shortcut
+        System.out.println("\"I love pizza\""); // \" shows " in print
+        System.out.println("\tIt tastes really good!"); // \t creates a tab for the print line
+
+        /*
+        *
+        * This is a multi line comment
+        *
+         */
+    }
+}
