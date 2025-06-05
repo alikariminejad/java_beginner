ae32879f77c9b22d352aed700bf8db93e7bdacd0
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-05 15:02:29 +0330

    expressions

---
+++ expressions/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+            // expressions = operands & operators
+            // operands = values, variables, numbers, quantity
+            // operators = + - * / %
+            int friends = 10;
+            friends = friends + 1; // = friends++
+            friends = friends -1; // = friends--
+        System.out.println(friends);
+        }
+    }
