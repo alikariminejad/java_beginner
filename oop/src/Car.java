5cb045a87db1830af9a9e0b26020232f770dd2fa
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-17 19:37:06 +0330

    oop

---
+++ oop/src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    // attributes
+    String make = "Chevrolet";
+    String model = "Ford";
+    int year = 2020;
+    String color = "Red";
+    double price = 50000.00;
+
+    // methods
+    void drive() {
+        System.out.println("Driving Car");
+    }
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+}
---
+++ oop/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // object = an instance of a class that may contain attributes and methods
+        // example: (phone, desk, computer, coffee cup)
+
+        Car mycar = new Car();
+        System.out.println(mycar.model); // = Ford
+        System.out.println(mycar.color); // = Red
+        mycar.drive(); // = Driving Car
+        mycar.brake(); // = You step on the brakes
+    }
+}
