77862493e502948c6cbd59f5fbb66eb421091515
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-17 22:28:56 +0330

    Array of objects

---
+++ toString_method/src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "Red";
+    int year = 2021;
+
+    public String toString(){
+        return make + " " + model + " " + color + " " + year;
+    }
+}
