ef11472b731b79c8aad7db9a041918f9edd6495e
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-17 21:44:55 +0330

    Variable Scope

---
+++ variable_scope/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // local variable = declared inside a method and visible only to that method
+
+        // global variable = declared outside a method, but within a class and visible to all parts of a class
+        DiceRoller diceRoller = new DiceRoller();
+    }
+}
