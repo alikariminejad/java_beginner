f34f29fe2ab7d01ada49acb5a81ab22ed92714b2
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-23 21:12:38 +0330

    Key Bindings

---
+++ KeyBindings/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Key Bindings = bind an action to a KeyStroke
+        //                don't require you to click a component to give it focus
+        //                all Swing components use Key Bindings
+        //                increased flexibility compared to KeyListeners
+        //                can assign Key strokes to individual Swing components
+        //                more difficult to utilize and set up
+        Game game = new Game();
+    }
+}
