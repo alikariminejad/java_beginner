a9de0e5a18f9198272dcbffc5cb43d19d71bb09e
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-24 01:02:12 +0330

    2D Graphics

---
+++ 2D_Graphics/src/MyPanel.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+
+    Image image;
+
+    MyPanel() {
+        image = new ImageIcon("painting.png").getImage();
+        this.setPreferredSize(new Dimension(500,500));
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawImage(image, 0, 0, null);
+        g2d.setPaint(Color.blue);
+        g2d.setStroke(new BasicStroke(5));
+        //g2d.drawLine(0,0,500,500);
+
+        g2d.setPaint(Color.pink);
+        //g2d.drawRect(0,0,200,200);
+        //g2d.fillRect(0,0,200,200);
+
+        g2d.setPaint(Color.orange);
+        g2d.drawOval(0,0,100,100);
+        g2d.fillOval(0,0,100,100);
+
+        g2d.drawArc(0,0,100,100,20,60);
+        g2d.fillArc(0,0,100,100,180,180);
+
+        int[] xPoints = {150,250,350};
+        int[] yPoints = {300,150,300};
+        g2d.drawPolygon(xPoints,yPoints,3);
+        g2d.fillPolygon(xPoints,yPoints,3);
+
+//        g2d.setPaint(Color.red);
+//        g2d.setFont(new Font("Ink Free", Font.BOLD, 50));
+//        g2d.drawString("U R A WINNER! :D", 40, 50);
+
+
+    }
+}
